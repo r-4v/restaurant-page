9ed6b27d3c26545cfcc1dc0295dd3ed2e698a7a4
r-4v <pravaspraveen777@gmail.com>
2021-10-19 20:59:38 +0530

    starter commit

---
+++ index.bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+body = document.querySelector('body');
+contentDiv = document.createElement('div');
+contentDiv.innerText = "whatsup";
+body.appendChild(contentDiv);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0EsNkIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYm9keSA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJ2JvZHknKTtcbmNvbnRlbnREaXYgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdkaXYnKTtcbmNvbnRlbnREaXYuaW5uZXJUZXh0ID0gXCJ3aGF0c3VwXCI7XG5ib2R5LmFwcGVuZENoaWxkKGNvbnRlbnREaXYpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
